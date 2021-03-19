cae4b608343f592edc4cf79c4b6fb8b6baf87905
EMRE CAN UĞURLU <emre1907.ecuu@gmail.com>
2021-03-19 13:19:36 +0300

    19 Mart 2021 Cuma

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Core.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Rental> GetId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Rental rental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
